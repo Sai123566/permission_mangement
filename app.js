46fed8b47bca669e9d9004942babba85432107e8
Sai123566 <149222091+Sai123566@users.noreply.github.com>
2025-03-11 19:15:20 +0530

    app.js

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ const axios = require('axios'); // Import axios for API requests
 
 const app = express();
 
-const serviceAccount = require("./serviceAccountKey.json");
-
-
-
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: "service_account",
+    project_id: process.env.project_id,
+    private_key_id: process.env. private_key_id,
+    private_key: process.env.private_key(/\\n/g, '\n'), // Ensure correct format
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.client_email}`
+  })
 });
+
 
 const db = admin.firestore();
 
