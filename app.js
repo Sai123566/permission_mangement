7bdb6935bcfb6d1220fd410c4a3ea89ce8f0bd24
Sai123566 <149222091+Sai123566@users.noreply.github.com>
2025-03-11 19:17:23 +0530

    app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ admin.initializeApp({
     type: "service_account",
     project_id: process.env.project_id,
     private_key_id: process.env. private_key_id,
-    private_key: process.env.private_key(/\\n/g, '\n'), // Ensure correct format
+    private_key: process.env.private_key.replace(/\\n/g, '\n'), // Ensure correct format
     client_email: process.env.client_email,
     client_id: process.env.client_id,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
